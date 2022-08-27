fee9699c699cb566cf6288204c4c8a4761944099
Chaos <antonioscutari2@hotmail.com>
2022-08-27 19:54:49 -0300

    [DONE] Routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,12 @@
-const { Router } = require('express');
+const { Router } = require("express");
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
+const pkRoute = require("./pokeRoute");
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-
+router.use("/", pkRoute);
 
 module.exports = router;
