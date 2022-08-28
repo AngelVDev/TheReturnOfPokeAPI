a2cc07d11eebf5ee25d9594604133aacfe1517e2
Chaos <antonioscutari2@hotmail.com>
2022-08-28 09:20:07 -0300

    [ADD] 'Created in Database'

--- src/models/Pokemon.js
+++ src/models/Pokemon.js
@@ -40,6 +40,10 @@ module.exports = (sequelize) => {
         type: DataTypes.INTEGER,
         allowNull: false,
       },
+      createdInDb: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: true,
+      },
     },
     { timestamps: false }
   );
