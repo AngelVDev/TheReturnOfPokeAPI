7fc72ee57b684fba9340650b18f7c816d4140ec2
Chaos <antonioscutari2@hotmail.com>
2022-09-14 06:15:47 -0300

    [TRY] #5

--- index.js
+++ index.js
@@ -19,7 +19,7 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
-const PORT = process.env.PORT;
+const PORT = `0.0.0.0:$PORT`;
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
   server.listen(PORT || 3001, () => {
