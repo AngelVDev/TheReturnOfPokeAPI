87cda5da19bdcf4fe6b12c9ff5e76cb1f8b3b371
Chaos <antonioscutari2@hotmail.com>
2022-09-16 23:35:45 -0300

    [TRY] Apicache

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,15 @@
 const { Router } = require("express");
+const apicache = require("apicache");
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 const pkRoute = require("./pokeRoute");
 const pkTypeRoute = require("./pokeTypeRoute");
 const router = Router();
+const cache = apicache.middleware;
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-router.use("/", pkRoute);
+router.use("/", cache("5 minutes"), pkRoute);
 router.use("/", pkTypeRoute);
 
 module.exports = router;
